4a43b776122b7dc7b442f35f44215d9601506dd8
shanoot <mattias.juhl@gmail.com>
2016-01-06 17:50:06 +1100

    First attemp at Schenk's BGN model, comparison to PV lighthouse's model demonstrates a 3 mV difference. Unsure what the casue is. Some cleaning up of the inputs and documentation is still required

--- matterial/bandgap.py
+++ matterial/bandgap.py
@@ -4,8 +4,8 @@ import os
 import ConfigParser
 
 from semiconductor.helper.helper import HelperFunctions
-import semiconductor.matterial.intrinsicbandgapmodels as iBg
-import semiconductor.matterial.BandgapNarrowingModels as Bgn
+import semiconductor.matterial.intrinsic_bandgap_models as iBg
+import semiconductor.matterial.bandgap_narrowing_models as Bgn
 
 
 class BandGap():
@@ -16,9 +16,9 @@ class BandGap():
     '''
 
     def __init__(self,
-                 matterial='Si', Egi_model=None, BNG_model=None, dopant=None):
+                 matterial='Si', iEg_model=None, BNG_model=None, dopant=None):
 
-        self.Egi = IntrinsicBandGap(matterial, model=Egi_model)
+        self.iEg = IntrinsicBandGap(matterial, model=iEg_model)
         self.BGN = BandGapNarrowing(matterial, model=BNG_model)
         self.dopant = dopant
 
@@ -34,15 +34,19 @@ class BandGap():
             dopant = self.dopant
 
         # just prints a warning if the model is for the incorrect dopants
-        dopant_model_list = self.BGN.available_models('dopant', dopant)
+        dopant_model_list = self.Egi.available_models('dopant', dopant)
         if self.BGN.model not in dopant_model_list:
             print 'You have the incorrect model for your dopant'
 
-        # print 'The band gaps are:', self.Egi.update_Eg(temp),
+        # print 'The band gaps are:', self.iEg.update_Eg(temp),
         # self.BGN.update_BGN(doping, min_car_den)
-        Eg = self.Egi.update_iEg(
+        Eg = self.iEg.update_iEg(
             temp) - self.BGN.update_BGN(doping, min_car_den)
         return Eg
+
+    def check_models(self):
+        self.iEg.check_models()
+        self.BGN.check_models()
 
 
 class IntrinsicBandGap(HelperFunctions):
@@ -112,7 +116,7 @@ class IntrinsicBandGap(HelperFunctions):
 
         data = np.genfromtxt(test_file, delimiter=',', names=True)
 
-        for temp,name in zip(data.dtype.names[0::2],data.dtype.names[1::2]):
+        for temp, name in zip(data.dtype.names[0::2], data.dtype.names[1::2]):
             plt.plot(
                 data[temp], data[name], '--', label=name)
 
@@ -148,12 +152,18 @@ class BandGapNarrowing(HelperFunctions):
         self.Models.read(constants_file)
         self.change_model(model)
 
-    def update_BGN(self, doping, min_car_den=None, model=None):
+    def update_BGN(self, doping, min_car_den=None, model=None, **kwargs):
+        '''
+        When finished should taken inputs:
+        Na, Nd, delta n, temp, 
+
+        then provide impurity, net doping, ne, nh, temp to the models.
+        '''
 
         if model is not None:
             self.change_model(model)
 
-        return getattr(Bgn, self.model)(self.vals, doping, min_car_den)
+        return getattr(Bgn, self.model)(self.vals, doping, min_car_den, **kwargs)
 
     def check_models(self):
         plt.figure('Bandgap narrowing')
@@ -161,7 +171,9 @@ class BandGapNarrowing(HelperFunctions):
 
         for model in self.available_models():
             BGN = self.update_BGN(doping, model=model)
-            plt.plot(doping, BGN, label=model)
+
+            if not np.all(BGN == 0):
+                plt.plot(doping, BGN, label=model)
 
         test_file = os.path.join(
             os.path.dirname(os.path.realpath(__file__)),
@@ -171,32 +183,11 @@ class BandGapNarrowing(HelperFunctions):
         print data.dtype.names
         for name in data.dtype.names[1:]:
             plt.plot(
-                data['N'], data[name], 'r--', label='PV-lighthouse\'s: ' + name)
+                data['N'], data[name], 'r--',
+                label='PV-lighthouse\'s: ' + name)
 
         plt.semilogx()
         plt.xlabel('Doping (cm$^{-3}$)')
         plt.ylabel('Bandgap narrowing (K)')
 
         plt.legend(loc=0)
-
-
-if __name__ == "__main__":
-    # a = IntrinsicCarrierDensity()
-    # a._PlotAll()
-    # plt.show()
-
-    a = BandGap('Si')
-
-    IntrinsicBandGap().check_models()
-    BandGapNarrowing().check_models()
-    plt.show()
-    # a.print_model_notes()
-    # deltan = np.logspace(12, 17)
-    # print a.Radiative.ni, a.Auger.ni
-    # doping = np.logspace(12, 20, 100)
-    # a.plot_all_models('update_BGN', xvalues=doping, doping=doping)
-
-    # a.caculate_Eg(300., 1e16, 1e16, 'boron')
-    # print plt.plot(doping, a.update_BGN(doping))
-    # plt.semilogx()
-    # plt.show()
